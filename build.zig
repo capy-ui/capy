8b1e9a96d6f5d192a2445eb53ca041d7cb7aeb16
zigster64 <steve@raffmail.net>
2022-01-06 14:12:10 +1000

    Add FreeBSD support
    - simply a matter of adding .freebsd to the 2 places where
      .linux is applied
    - uses the same Gtk libs as the linux version
    - tested, builds and runs all demos out of the box

--- build.zig
+++ build.zig
@@ -4,9 +4,9 @@ pub fn install(step: *std.build.LibExeObjStep, comptime prefix: []const u8) !voi
     step.subsystem = .Native;
     // step.linkSystemLibrary("glfw");
     // step.linkSystemLibrary("GLESv2");
-    
+
     switch (step.target.getOsTag()) {
-        .linux => {
+        .linux, .freebsd => {
             step.linkLibC();
             step.linkSystemLibrary("gtk+-3.0");
         },
@@ -17,7 +17,7 @@ pub fn install(step: *std.build.LibExeObjStep, comptime prefix: []const u8) !voi
             switch (step.target.toTarget().cpu.arch) {
                 .x86_64 => step.addObjectFile(prefix ++ "/src/backends/win32/res/x86_64.o"),
                 //.i386 => step.addObjectFile(prefix ++ "/src/backends/win32/res/i386.o"), // currently disabled due to problems with safe SEH
-                else => {} // not much of a problem as it'll just lack styling
+                else => {}, // not much of a problem as it'll just lack styling
             }
         },
         .freestanding => {
@@ -31,14 +31,10 @@ pub fn install(step: *std.build.LibExeObjStep, comptime prefix: []const u8) !voi
             // TODO: use the GLES backend as long as the windowing system is supported
             // but the UI library isn't
             return error.UnsupportedOs;
-        }
+        },
     }
 
-    const zgt = std.build.Pkg {
-        .name = "zgt",
-        .path = std.build.FileSource.relative(prefix ++ "/src/main.zig"),
-        .dependencies = &[_]std.build.Pkg{}
-    };
+    const zgt = std.build.Pkg{ .name = "zgt", .path = std.build.FileSource.relative(prefix ++ "/src/main.zig"), .dependencies = &[_]std.build.Pkg{} };
 
     step.addPackage(zgt);
 }
@@ -54,16 +50,16 @@ pub fn build(b: *std.build.Builder) !void {
     defer walker.deinit();
     while (try walker.next()) |entry| {
         if (entry.kind == .File and std.mem.eql(u8, std.fs.path.extension(entry.path), ".zig")) {
-            const name = try std.mem.replaceOwned(u8, b.allocator, entry.path[0..entry.path.len-4], "/", "-");
+            const name = try std.mem.replaceOwned(u8, b.allocator, entry.path[0 .. entry.path.len - 4], "/", "-");
             defer b.allocator.free(name);
 
             // it is not freed as the path is used later for building
             const programPath = try std.mem.concat(b.allocator, u8, &.{ "examples/", entry.path });
 
             const exe: *std.build.LibExeObjStep = if (target.toTarget().isWasm())
-                    b.addSharedLibrary(name, programPath, .unversioned)
-                else
-                    b.addExecutable(name, programPath);
+                b.addSharedLibrary(name, programPath, .unversioned)
+            else
+                b.addExecutable(name, programPath);
             exe.setTarget(target);
             exe.setBuildMode(mode);
             try install(exe, ".");
